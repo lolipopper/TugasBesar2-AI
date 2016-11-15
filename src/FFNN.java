9a7c1768f7799dccb680593ccd8cde801d70e7ed
lolipopper <njruntuwene@gmail.com>
2016-11-15 23:07:45 +0900

    Initial Commit

---
+++ src/FFNN.java
@@ -0,0 +1,13 @@
+/**
+ * Created by njruntuwene on 11/15/16.
+ */
+
+import weka.classifiers.*;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class FFNN extends AbstractClassifier {
+    public void buildClassifier(Instances data) {
+
+    }
+}
