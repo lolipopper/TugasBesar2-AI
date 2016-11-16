04b7afaef0388c7741fdddadec060486ce6d06b8
lolipopper <njruntuwene@gmail.com>
2016-11-17 04:32:50 +0900

    Add Test

--- test/FFNNTest.java
+++ test/FFNNTest.java
@@ -2,16 +2,31 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+import java.io.*;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by njruntuwene on 11/17/16.
- */
 public class FFNNTest {
+    private Classifier classifier;
+    private Instances data;
 
     @Before
     public void setUp() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String filename = "~/Desktop/iris.arff";
+        DataSource source = new DataSource(filename);
+        data = source.getDataSet();
+
+        if (data.classIndex() == -1)
+            data.setClassIndex(data.numAttributes() - 1);
+
+        classifier = new FFNN();
+        classifier.buildClassifier(data);
     }
 
     @After
@@ -41,7 +56,7 @@ public class FFNNTest {
 
     @Test
     public void testBuildClassifier() throws Exception {
-
+        classifier.buildClassifier(data);
     }
 
     @Test
@@ -56,6 +71,11 @@ public class FFNNTest {
 
     @Test
     public void testBackPropagation() throws Exception {
+
+    }
+
+    @Test
+    public void testRun() throws Exception {
 
     }
 }
