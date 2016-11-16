196d3946e309c21ef5decb86e1fc7df2b470327d
lolipopper <njruntuwene@gmail.com>
2016-11-17 03:07:12 +0900

    Add basic algorithm to buildClassifier

--- src/FFNN.java
+++ src/FFNN.java
@@ -3,20 +3,35 @@
  */
 
 import weka.classifiers.*;
+import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
+
+import java.util.Enumeration;
 
 public class FFNN extends AbstractClassifier {
     private double[] inputLayer;
     private double[] hiddenLayer;
     private double[] outputLayer;
 //    private int[][] inputToOutputWeight;
-    private int[][] inputToHiddenWeight;
-    private int[][] hiddenToOutputWeight;
+    private double[][] inputToHiddenWeight;
+    private double[][] hiddenToOutputWeight;
     private Instances instances;
     private int numClasses;
     private int numAttributes;
-    private double[][] targetValue;
+    private int numHiddenNode;
+    private double[] targetValue;
+    private double minError;
+
+    public void setInputLayer(Instance instance) {
+        int attIndex = 0;
+        for (Enumeration enu = instance.enumerateAttributes(); enu.hasMoreElements(); attIndex++) {
+            Attribute attr = (Attribute) enu.nextElement();
+
+            double val = instance.value(attr);
+            inputLayer[attIndex] = val;
+        }
+    }
 
     public double sigmoid(int x) {
         return 1/(1+Math.exp(-x));
@@ -49,7 +64,29 @@ public class FFNN extends AbstractClassifier {
     }
 
     public void buildClassifier(Instances data) {
+        numClasses = data.numClasses();
+        numAttributes = data.numAttributes();
+        numHiddenNode = numClasses + numAttributes + 1;
+        inputLayer = new double[numAttributes];
+        hiddenLayer = new double[numHiddenNode];
+        outputLayer = new double[numClasses];
+        inputToHiddenWeight = new double[numAttributes][numHiddenNode];
+        hiddenToOutputWeight = new double[numHiddenNode][numClasses];
+        instances = new Instances(data);
+        targetValue = new double[numClasses];
+        minError = 0.01;
 
+        while (calculateCumulativeError() > minError) {
+            for (int i = 0; i < instances.numInstances(); i++) {
+                Instance curInstance = instances.instance(i);
+
+                setInputLayer(curInstance);
+                calculateHiddenLayer();
+                calculateOutputLayer();
+
+                backPropagation();
+            }
+        }
     }
 
     public double calculateCumulativeError() {
