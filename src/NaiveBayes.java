b5b3f5e1375e0dcc427bf50b9e5a20f2f6b77703
HafizhHD <13514072@std.stei.itb.ac.id>
2016-11-20 16:59:39 +0700

    NB Finished

--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -21,19 +21,32 @@ public class NaiveBayes extends AbstractClassifier {
 	//indeks 3: banyak jenis value dari atribut kelas
 	public double[][][] probAttrNonClass;
 	
-    
-    public void buildClassifier(Instances data) {
+	public Instances instances;
+	public int numClasses;
+	public int numAttr;
+	
+	public Instances getInstances() {
+		return instances;
+	}
+	
+	public int getNumAttr() {
+		return numAttr;
+	}
 
-    }
+	public int getNumClasses() {
+		return numClasses;
+	}
+	
+	public NaiveBayes() {};
     
     //get nilai maksimal dari value semua atribut non kelas (untuk keperluan array)
-	public int getMaxAttrValCount(Instances train) {
+	public int getMaxAttrValCount() {
 		int n = 0;
-		int countAttrNonClass = train.numAttributes()-1;
+		int countAttrNonClass = instances.numAttributes()-1;
 		for (int i=0;i<countAttrNonClass;i++) {
-			int countAttrVal = train.attribute(i).numValues();
+			int countAttrVal = instances.attribute(i).numValues();
 			for (int j=0;j<countAttrVal;j++) {
-				int countVal = countValAppears(train,i,train.attribute(i).value(j));
+				int countVal = countValAppears(i,instances.attribute(i).value(j));
 				if(n<countVal) {
 					n = countVal;
 				}
@@ -42,11 +55,22 @@ public class NaiveBayes extends AbstractClassifier {
 		return n;
 	}
 	
-    //get banyaknya kemunculan value pada semua instances
-	public int countValAppears(Instances train, int attr, String val) {
+    //get frekuensi kemunculan value pada semua instances
+	public int countValAppears(int attr, String val) {
 		int res = 0;
-		for (int i=0;i<train.numInstances();i++) {
-			if (train.instance(i).stringValue(train.attribute(attr)).equals(val)) {
+		for (int i=0;i<instances.numInstances();i++) {
+			if (instances.instance(i).stringValue(instances.attribute(attr)).equals(val)) {
+				res++;
+			}
+		}
+		return res;
+	}
+	
+	//get frekuensi kemunculan value kelas saat kondisi value attr non kelas=val
+	public int countValWithCond(int attr, String val, String clas) {
+		int res = 0;
+		for (int i=0;i<instances.numInstances();i++) {
+			if (instances.instance(i).stringValue(instances.classAttribute()).equals(clas) && instances.instance(i).stringValue(instances.attribute(attr)).equals(val)) {
 				res++;
 			}
 		}
@@ -55,26 +79,26 @@ public class NaiveBayes extends AbstractClassifier {
 	
     //set probabilitas kemunculan value atribut kelas per semua instance
     //baru sadar bisa pakai fungsi yang atas
-	public void setProbPerClass(Instances train) {
-		int countClassVal = train.classAttribute().numValues();
+	public void setProbPerClass() {
+		int countClassVal = instances.classAttribute().numValues();
 		probClass = new double[countClassVal];
 		for (int n=0;n<countClassVal;n++) {
 			int countPerClass = 0;
-			for (int i=0;i<train.numInstances();i++) {
-				if (train.instance(i).stringValue(train.classAttribute()).equals(train.classAttribute().value(n))) {
+			for (int i=0;i<instances.numInstances();i++) {
+				if (instances.instance(i).stringValue(instances.classAttribute()).equals(instances.classAttribute().value(n))) {
 					countPerClass++;
 				}
 			}
-			probClass[n] = (double)countPerClass/train.numInstances();
+			probClass[n] = (double)countPerClass/instances.numInstances();
 		}
 	}
 	
-    //BELUM JADI
+    //BELUM DITES
     //set conditional probability untuk setiap value dari setiap atribut non kelas
-	public void setCondProbPerAttr(Instances train) {
-		int countClassVal = train.classAttribute().numValues();
-		int countAttrNonClass = train.numAttributes()-1;
-		int countAttrVal = getMaxAttrValCount(train);
+	public void setCondProbPerAttr() {
+		int countClassVal = instances.classAttribute().numValues();
+		int countAttrNonClass = instances.numAttributes()-1;
+		int countAttrVal = getMaxAttrValCount();
 		
 		probAttrNonClass = new double[countAttrNonClass][countAttrVal][countClassVal];
 		
@@ -82,24 +106,28 @@ public class NaiveBayes extends AbstractClassifier {
 		for (int i=0;i<countAttrNonClass;i++) {
 			for (int j=0;j<countAttrVal;j++) {
 				for (int k=0;k<countClassVal;k++) {
-					probAttrNonClass[i][j][k] = -999; //value that won't be accessed, initiated by -999
+					probAttrNonClass[i][j][k] = -1; //value that won't be accessed, initiated by -999
 				}
 			}
 		}
 		
-		//belum jadi
-		/*int countAttr = 0;
-		for (ArrayList arr : probAttrNonClass) {
-			for (int n=0;n<countClassVal;n++) {
-				int countAttrAppears = 0;
-				int countPerClass = 0;
-				for (int i=0;i<train.numInstances();i++) {
-					if (train.instance(i).stringValue(train.attribute(countAttr)).equals(train.attribute(countAttr).value(n))) {
-						countAttr++;
-					}
+		//belum dites
+		for (int m=0;m<countAttrNonClass;m++) {
+			for (int n=0;n<countAttrVal;n++) {
+				for (int p=0;p<countClassVal;p++) {
+					int a = countValAppears(m,instances.attribute(m).value(n));
+					int b = countValWithCond(m,instances.attribute(m).value(n),instances.classAttribute().value(p));
+					probAttrNonClass[m][n][p] = b/a;
 				}
-				arr.add((double)countPerClass/train.numInstances());
 			}
-		}*/
+		}
+	}
+	
+	public void buildClassifier(Instances data) {
+		instances = new Instances(data);
+		numClasses = data.numClasses();
+		numAttr = data.numAttributes();
+		setProbPerClass();
+		setCondProbPerAttr();
 	}
 }
